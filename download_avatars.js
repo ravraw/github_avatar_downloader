0a567e47886a20219836f85275b0e90dd8d3aa5f
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-29 16:05:00 -0600

    added cb and request options

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,20 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url:
+      'https://api.github.com/repos/' +
+      repoOwner +
+      '/' +
+      repoName +
+      '/contributors',
+    headers: { 'User-Agent': 'request' }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors();
